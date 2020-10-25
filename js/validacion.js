49fef9a417e7f9ceebc0282dc7b8fb5e5c5817be
unknown <julietaextra14@gmail.com>
2020-10-25 15:49:17 -0300

    se creo la funcion send y  checkValidacion

--- js/validacion.js
+++ js/validacion.js
@@ -1,5 +1,29 @@
 function startValidacion(){
 
 }
+function send(){
+
+}
+function checkValidacion(){
+    var nombre, apellido , dni , email , domicilio;
+    nombre = document.getElementById("nombre");
+    apellido = document.getElementById("apellido");
+    dni = document.getElementById("dni");
+    domicilio = document.getElementById("domicilio");
+    email =  nombre = document.getElementById("email");
+
+    if(dni.length!=8){
+       alert("dni debe tener 8 numeros");
+    }else{
+       alert("correcto");
+    }
+    if(dni>0 && dni<9){
+
+    }else{
+        alert("ingrese solo numeros")
+    }
+
+
+}
 
 window.addEventListener("load" , startValidacion , false);
